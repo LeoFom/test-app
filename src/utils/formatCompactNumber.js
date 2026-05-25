65fb1d911503da67417d703cfdd6f995f2b269dc
Leonid Fomenko <leonidfomenko.ua@gmail.com>
2026-05-25 23:14:34 +0300

    feat(utils): add formatCompactNumber function

---
+++ src/utils/formatCompactNumber.js
@@ -0,0 +1,10 @@
+export function formatCompactNumber(value) {
+  if (value == null) return "-";
+
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    notation: "compact",
+    maximumFractionDigits: 2,
+  }).format(value);
+}
