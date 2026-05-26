89527367e7a1310ce2e5473c1d93e30856b8b403
Leonid Fomenko <leonidfomenko.ua@gmail.com>
2026-05-26 12:15:23 +0300

    feat(constants): add COIN_OPTIONS data

---
+++ src/constants/coins.js
@@ -0,0 +1,14 @@
+export const COIN_OPTIONS = [
+  {
+    label: "Bitcoin",
+    value: "bitcoin",
+  },
+  {
+    label: "Ethereum",
+    value: "ethereum",
+  },
+  {
+    label: "Dogecoin",
+    value: "dogecoin",
+  },
+];
